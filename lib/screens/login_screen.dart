5192bdb475c290f2518cde429aa0cacd5b846fbb
YoussefEmad02 <yousefemad2002@icloud.com>
2024-12-01 22:43:21 +0200

    All features done except editing and ui enhancment

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -18,68 +18,122 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+      appBar: AppBar(
+        title: Text("Muscle Temple Gym"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        elevation: 2,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Email Field
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: "Email"),
-                validator: Validators.validateEmail,
-              ),
-              SizedBox(height: 10),
-              // Password Field
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: "Password"),
-                obscureText: true,
-                validator: Validators.validatePassword,
+              // Circular Avatar for Logo
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/logo.jpg'), // Replace with your logo asset path
+                backgroundColor: Colors.blueAccent.shade100,
               ),
               SizedBox(height: 20),
-              // Error Message
-              if (_errorMessage != null) ...[
-                Text(_errorMessage!, style: TextStyle(color: Colors.red)),
-                SizedBox(height: 10),
-              ],
-              // Login Button
-              ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    String? result = await AuthService()
-                        .login(_emailController.text, _passwordController.text);
-                    if (result == null) {
-                      // Navigate to Workout Plan History Screen
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => WorkoutHistoryScreen(
-                            userId: AuthService().currentUser?.uid ?? '',
-                          ),
-                        ),
-                      );
-                    } else {
-                      setState(() {
-                        _errorMessage = result;
-                      });
-                    }
-                  }
-                },
-                child: Text("Log In"),
+              Text(
+                "Welcome Back!",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              // Navigate to Sign-Up
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUpScreen()),
-                  );
-                },
-                child: Text("Don't have an account? Sign up"),
+              SizedBox(height: 10),
+              Text(
+                "Please log in to continue",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              SizedBox(height: 30),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    // Email Field
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email),
+                      ),
+                      validator: Validators.validateEmail,
+                    ),
+                    SizedBox(height: 15),
+                    // Password Field
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      obscureText: true,
+                      validator: Validators.validatePassword,
+                    ),
+                    SizedBox(height: 20),
+                    // Error Message
+                    if (_errorMessage != null) ...[
+                      Text(
+                        _errorMessage!,
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                    // Login Button
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          String? result = await AuthService().login(
+                            _emailController.text,
+                            _passwordController.text,
+                          );
+                          if (result == null) {
+                            // Navigate to Workout Plan History Screen
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => WorkoutHistoryScreen(
+                                  userId: AuthService().currentUser?.uid ?? '',
+                                ),
+                              ),
+                            );
+                          } else {
+                            setState(() {
+                              _errorMessage = result;
+                            });
+                          }
+                        }
+                      },
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    // Navigate to Sign-Up
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignUpScreen()),
+                        );
+                      },
+                      child: Text(
+                        "Don't have an account? Sign up",
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
